c81f295b1e7fe06dbee1e7ffd46bdd0829a52ca5
Chris Scheller <schelcj@pobox.com>
2012-04-09 11:59:58 -0400

    * had to change the json format to accomadate datatable * use the file name as the faculty members name

--- bin/parse.pl
+++ bin/parse.pl
@@ -9,9 +9,11 @@ use File::Slurp qw(read_dir write_file);
 use Data::Dumper;
 use Readonly;
 use JSON;
+use Text::Autoformat;
 
 Readonly::Scalar my $DATA_DIR  => qq{$Bin/../data};
 Readonly::Scalar my $JSON_FILE => qq{$DATA_DIR/publications.json};
+Readonly::Scalar my $JSON_VAR  => q{aaData};
 Readonly::Array  my @COLUMNS   => (qw(title author journal volume number pages year publisher));
 
 my $json         = JSON->new();
@@ -22,18 +24,25 @@ foreach my $export (@exports) {
   my $file = File::PathInfo->new();
   $file->set(qq{$DATA_DIR/$export});
 
-  my $fh    = IO::File->new($file->abs_path());
-  my $bib   = BibTeX::Parser->new($fh);
-  my @pubs  = ();
+  my $fh   = IO::File->new($file->abs_path());
+  my $bib  = BibTeX::Parser->new($fh);
+  my $name = clean_name($file->filename_only());
 
   while (my $entry = $bib->next) {
-    push @pubs, {map {$_ => $entry->field($_)} @COLUMNS};
+    my @pub = map {$entry->field($_)} @COLUMNS;
+    unshift @pub, $name;
+    push @publications, \@pub;
   }
-
-  push @publications, {
-    name => $file->filename_only,
-    pubs => \@pubs,
-  };
 }
 
-write_file($JSON_FILE, $json->pretty->encode(\@publications));
+write_file($JSON_FILE, $json->encode({$JSON_VAR => \@publications}));
+
+sub clean_name {
+  my ($name) = @_;
+
+  $name =~ s/\_/ /g;
+  $name = autoformat $name, {case => 'title'};
+  $name =~ s/[\r\n]//g;
+
+  return $name;
+}
