dc193dfe75f7937fdb0a1653cd4a5c4b3973ad34
Chris Scheller <schelcj@pobox.com>
2012-04-18 11:21:16 -0400

    changed up what is written out so that we can present a list of faculty members and then display only that persons publications.

--- bin/parse.pl
+++ bin/parse.pl
@@ -11,31 +11,39 @@ use Readonly;
 use JSON;
 use Text::Autoformat;
 
-Readonly::Scalar my $DATA_DIR  => qq{$Bin/../data};
-Readonly::Scalar my $JSON_FILE => qq{$DATA_DIR/publications.json};
-Readonly::Scalar my $JSON_VAR  => q{aaData};
-Readonly::Array  my @COLUMNS   => (qw(title author journal volume number pages year publisher));
+Readonly::Scalar my $DATA_DIR => qq{$Bin/../data};
+Readonly::Scalar my $JSON_DIR => qq{$Bin/../public/json};
+Readonly::Scalar my $FAC_JSON => qq{$JSON_DIR/faculty.json};
+Readonly::Scalar my $JSON_VAR => q{aaData};
 
-my $json         = JSON->new();
-my @exports      = read_dir($DATA_DIR);
-my @publications = ();
+Readonly::Array my @COLUMNS => (qw(title author journal volume number pages year publisher));
+
+my $json        = JSON->new();
+my @exports     = read_dir($DATA_DIR);
+my $faculty_ref = {};
 
 foreach my $export (@exports) {
-  my $file = File::PathInfo->new();
+  my @publications = ();
+  my $file         = File::PathInfo->new();
+
   $file->set(qq{$DATA_DIR/$export});
 
-  my $fh   = IO::File->new($file->abs_path());
-  my $bib  = BibTeX::Parser->new($fh);
-  my $name = clean_name($file->filename_only());
+  next if $file->ext ne 'bib';
+
+  my $fh       = IO::File->new($file->abs_path());
+  my $bib      = BibTeX::Parser->new($fh);
+  my $filename = $file->filename_only();
 
   while (my $entry = $bib->next) {
-    my @pub = map {$entry->field($_)} @COLUMNS;
-    unshift @pub, $name;
-    push @publications, \@pub;
+    push @publications, [map {$entry->field($_)} @COLUMNS];
   }
+
+  write_file(qq[$JSON_DIR/$filename.json], $json->pretty->encode({$JSON_VAR => \@publications}));
+
+  $faculty_ref->{$filename} = clean_name($filename);
 }
 
-write_file($JSON_FILE, $json->encode({$JSON_VAR => \@publications}));
+write_file($FAC_JSON, $json->pretty->encode($faculty_ref));
 
 sub clean_name {
   my ($name) = @_;
