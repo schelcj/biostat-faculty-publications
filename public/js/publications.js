288c8b4444fa37795e24a6dfcd8e47357609891d
Chris Scheller <schelcj@pobox.com>
2012-04-20 14:46:25 -0400

    adding a link to the google scholar page by embedding the url in the option node

--- public/js/publications.js
+++ public/js/publications.js
@@ -1,26 +1,41 @@
 $(document).ready(function() {
   $.getJSON('json/faculty.json', function(data) {
     $.each(data, function(key, val) {
-      $('<option />', {value: key, text: val}).appendTo('#faculty_selector');
+      $('<option />', {value: key, text: val.name, url: val.url}).appendTo('#faculty_selector');
     });
   });
 
   $('#faculty_selector').change(function() {
+    $('<a />', {
+      href: $(':selected', this).attr('url'),
+      text: 'Google Scholar Profile'
+    }).appendTo($('#google_scholar_link').empty());
+
     $('#publications').dataTable({
+      // 'bScrollInfinite': true,
+      // 'bScrollCollapse': true,
+      // 'sScrollY':        '500px',
+      'bAutoWidth':  false,
       'bProcessing': true,
-      'sAjaxSource': 'json/' + $(this).val() + ".json",
       'bDestroy':    true,
+      'sAjaxSource': 'json/' + $(this).val() + ".json",
       'aaSorting': [[ 6, "desc" ]],
       'aoColumns': [
-        { 'sTitle': 'Title' },
-        { 'sTitle': 'Author' },
-        { 'sTitle': 'Journal' },
-        { 'sTitle': 'Volume' },
-        { 'sTitle': 'Number' },
-        { 'sTitle': 'Pages' },
-        { 'sTitle': 'Year' },
+        { 'sTitle': 'Title'     },
+        { 'sTitle': 'Author'    },
+        { 'sTitle': 'Journal'   },
+        { 'sTitle': 'Volume'    },
+        { 'sTitle': 'Number'    },
+        { 'sTitle': 'Pages'     },
+        { 'sTitle': 'Year'      },
         { 'sTitle': 'Publisher' }
       ],
+      'aoColumnDefs': [{
+        'aTargets':      ['_all'],
+        'fnCreatedCell': function (nTd, sData, oData, iRow, iCol) {
+            $(nTd).attr('title',sData);
+        }
+      }]
     });
   });
 });
