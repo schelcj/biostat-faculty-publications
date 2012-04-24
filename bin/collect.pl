85e44aaed05c5de70b7cf54f4f6be12114dd4cf6
Chris Scheller <schelcj@pobox.com>
2012-04-24 09:06:57 -0400

    added another faculty member that has unicode characters in his name which made me rethink the way i was handling filenames. refactored these quite a bit to be much simpler by using the google scholar id instead of the name.

--- bin/collect.pl
+++ bin/collect.pl
@@ -8,52 +8,25 @@ use File::Slurp qw(write_file);
 use Readonly;
 use URI;
 
-Readonly::Scalar my $BASE_URL        => 'http://scholar.google.com/citations?view_op=export_citations&hl=en&user=%s';
+Readonly::Scalar my $BASE_URL        => 'http://scholar.google.com/citations?view_op=export_citations&hl=en&user=';
 Readonly::Scalar my $EXPORT_FORMAT   => 0;
-Readonly::Scalar my $EXPORT_BTN_TEXT => q{Export all articles by %s};
+Readonly::Scalar my $EXPORT_BTN_TEXT => q{Export all articles by };
 
 Readonly::Array my @FACULTY => LoadFile(qq{$Bin/../config/faculty.yml});
 
 foreach my $member (@FACULTY) {
-  my $agent = get_agent();
-  my $uri   = get_export_uri($member->{gid});
-  my $post  = build_post_data($member->{name});
-
-  $agent->post($uri, $post);
-
-  store_publications($member->{name}, $agent->content);
-}
-
-sub get_agent {
+  my $uri   = URI->new($BASE_URL . $member->{gid});
   my $agent = WWW::Mechanize->new();
+
   $agent->agent_alias('Windows Mozilla');
-
-  return $agent;
-}
-
-sub get_export_uri {
-  my ($gid) = @_;
-  my $url = sprintf $BASE_URL, $gid;
-
-  return URI->new($url);
-}
-
-sub build_post_data {
-  my ($name) = @_;
-
-  return {
+  $agent->post($uri, {
     cit_fmt        => $EXPORT_FORMAT,
-    export_all_byn => sprintf($EXPORT_BTN_TEXT, $name),
-  };
-}
+    export_all_byn => $EXPORT_BTN_TEXT . $member->{name},
+  });
 
-sub store_publications {
-  my ($name, $export) = @_;
-
-  $name =~ s/\s+/_/g;
-  my $file = sprintf q{data/%s.bib}, lc($name);
-
-  write_file($file, $export);
-
-  return;
+  write_file(
+    qq{data/$member->{gid}.bib},
+    {binmode => ':utf8'},
+    $agent->content
+  );
 }
--- bin/parse.pl
+++ bin/parse.pl
@@ -11,12 +11,13 @@ use Readonly;
 use JSON;
 use Text::Autoformat;
 use YAML qw(LoadFile);
+use feature qw(unicode_strings);
 
 Readonly::Scalar my $DATA_DIR => qq{$Bin/../data};
 Readonly::Scalar my $JSON_DIR => qq{$Bin/../public/json};
 Readonly::Scalar my $FAC_JSON => qq{$JSON_DIR/faculty.json};
 Readonly::Scalar my $JSON_VAR => q{aaData};
-Readonly::Scalar my $BASE_URL => q{http://scholar.google.com/citations?user=%s};
+Readonly::Scalar my $BASE_URL => q{http://scholar.google.com/citations?user=};
 
 Readonly::Array my @FACULTY => LoadFile(qq{$Bin/../config/faculty.yml});
 Readonly::Array my @COLUMNS => (qw(title author journal volume number pages year));
@@ -35,36 +36,19 @@ foreach my $export (@exports) {
 
   my $fh       = IO::File->new($file->abs_path());
   my $bib      = BibTeX::Parser->new($fh);
-  my $filename = $file->filename_only();
-  my $member   = clean_name($filename);
+  my $gid      = $file->filename_only();
+  my ($member) = grep {$_->{gid} eq $gid} @FACULTY;
 
   while (my $entry = $bib->next) {
     push @publications, [map {$entry->field($_)} @COLUMNS];
   }
 
-  write_file(qq[$JSON_DIR/$filename.json], $json->pretty->encode({$JSON_VAR => \@publications}));
+  write_file(qq[$JSON_DIR/$gid.json], $json->pretty->encode({$JSON_VAR => \@publications}));
 
-  $faculty_ref->{$filename} = {
-    name => $member,
-    url  => get_scholar_url($member)
+  $faculty_ref->{$gid} = {
+    name => $member->{name},
+    url  => $BASE_URL . $gid,
   };
 }
 
 write_file($FAC_JSON, $json->pretty->encode($faculty_ref));
-
-sub clean_name {
-  my ($name) = @_;
-
-  $name =~ s/\_/ /g;
-  $name = autoformat $name, {case => 'title'};
-  $name =~ s/[\r\n]//g;
-
-  return $name;
-}
-
-sub get_scholar_url {
-  my ($name)        = @_;
-  my ($faculty_ref) = grep {$_->{name} =~ /$name/} @FACULTY;
-
-  return sprintf $BASE_URL, $faculty_ref->{gid};
-}
