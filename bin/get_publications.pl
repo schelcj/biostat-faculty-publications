fd7ac1b6305b3a8c78e6231d441d69ec4534c0df
Chris Scheller <schelcj@umich.edu>
2014-12-04 11:36:43 -0500

    final changes to clean up the author name field. switch to JSON as well.

--- bin/get_publications.pl
+++ bin/get_publications.pl
@@ -6,10 +6,10 @@ use Modern::Perl;
 use Mojo::UserAgent;
 use URI;
 use URI::QueryParam;
-use JSON::Any;
-use File::Slurp qw(write_file);
+use JSON;
+use File::Slurp qw(write_file read_file);
 use Biostat::LDAP qw(get_umod_realname);
-use Data::Dumper;
+use Text::Names qw(cleanName samePerson reverseName);
 
 my $FACULTY_URL = q{http://www.sph.umich.edu/iscr/faculty/dept.cfm?deptID=1};
 my $PUB_URL     = q{https://www.umms.med.umich.edu/profile/publications};
@@ -17,11 +17,21 @@ my $CSS         = q{#main-content > h2:nth-child(9) ~ p > a:nth-child(1)};
 my @uniqnames   = get_faculty_uniqnames($FACULTY_URL, $CSS);
 
 say 'Saving uniqnames';
-write_file(qq{$Bin/../public/json/faculty.json}, JSON::Any->to_json(\@uniqnames));
+write_file(qq{$Bin/../public/json/faculty.json}, to_json(\@uniqnames, {pretty => 1, utf8 => 1}));
 
 for my $name (@uniqnames) {
   say "Fetching publications for $name->{uniqname}";
   get_faculty_publications($name->{uniqname});
+
+  my $pubs = clean_author_names($name);
+  my $file = get_faculty_json($name->{uniqname});
+  my $json = to_json($pubs, {pretty => 1, utf8 => 1});
+  write_file($file, $json);
+}
+
+sub get_faculty_json {
+  my ($uniqname) = @_;
+  return qq{$Bin/../public/json/faculty/$uniqname.json};
 }
 
 sub get_faculty_uniqnames {
@@ -43,17 +53,33 @@ sub get_faculty_uniqnames {
   return @names;
 }
 
-sub get_publication_url {
-  my ($uniqname) = @_;
-  return "$PUB_URL/$uniqname.json";
-}
-
 sub get_faculty_publications {
   my ($uniqname) = @_;
 
-  my $url   = get_publication_url($uniqname);
+  my $url   = qq{$PUB_URL/$uniqname.json};
   my $agent = Mojo::UserAgent->new();
   my $page  = $agent->get($url);
+  my $json  = get_faculty_json($uniqname);
 
-  $page->res->content->asset->move_to(qq{$Bin/../public/json/faculty/$uniqname.json});
+  $page->res->content->asset->move_to($json);
+}
+
+sub clean_author_names {
+  my ($name) = @_;
+
+  my $fac_json     = get_faculty_json($name->{uniqname});
+  my $publications = from_json(read_file($fac_json), {utf8 => 1});
+  my $realname     = reverseName(cleanName($name->{realname}));
+  my @articles     = @{$publications->{publications}->{article}};
+  my $count        = ($publications->{publications}->{count} - 1);
+
+  for my $i (0 .. $count) {
+    my @authors = split(/; /, $articles[$i]->{author});
+    my @cleaned_authors = map {(samePerson($realname, $_)) ? $realname : $_} map {reverseName(cleanName($_))} @authors;
+
+    $publications->{publications}->{article}->[$i]->{clean_author} = join('; ', @cleaned_authors);
+    $publications->{umod_realname} = $realname;
+  }
+
+  return $publications;
 }
