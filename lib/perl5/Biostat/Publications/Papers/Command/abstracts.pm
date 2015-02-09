298588f9bad9462a1932ae26937260f4371095cb
Chris Scheller <schelcj@pobox.com>
2015-02-09 13:11:59 -0500

    import abstracts from pubmed

--- lib/perl5/Biostat/Publications/Papers/Command/abstracts.pm
+++ lib/perl5/Biostat/Publications/Papers/Command/abstracts.pm
@@ -1,7 +1,18 @@
 package Biostat::Publications::Papers::Command::abstracts;
 
+# Guidelines for pubmed API:
+#
+# In order not to overload the E-utility servers, NCBI recommends that
+# users post no more than three URL requests per second and limit large jobs
+# to either weekends or between 9:00 PM and 5:00 AM Eastern time during
+# weekdays.
+
 use Biostat::Publications::Papers -command;
 use Biostat::Publications::Base qw(biostat www);
+
+Readonly::Scalar my $ALIAS          => q{Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36};
+Readonly::Scalar my $PUBMED_URL_FMT => q{http://www.ncbi.nlm.nih.gov/pubmed/%d?report=xml&format=text};
+Readonly::Scalar my $XPATH          => q{//PubmedArticle/MedlineCitation/Article/Abstract/AbstractText};
 
 sub opt_spec {
   return (['import|i', 'Import abstracts from pubmed'],);
@@ -11,12 +22,23 @@ sub execute {
   my ($self, $opt, $args) = @_;
 
   if ($opt->{import}) {
-    my $db = Biostat::Publications::DB->new();
+    my $db    = Biostat::Publications::DB->new();
+    my $agent = Mojo::UserAgent->new;
 
     for my $pub ($db->resultset('Publication')->all) {
-      print Dumper $pub->pmid->abstract;
-    }
+      next unless $pub->pmid;
 
+      unless ($pub->abstracts->count) {
+        my $url      = sprintf $PUBMED_URL_FMT, $pub->pmid;
+        my $content  = $agent->get($url)->res->body;
+        my $parser   = XML::XPath->new(xml => $content);
+        my $abstract = join($EMPTY, map {$_->string_value} $parser->find($XPATH)->get_nodelist);
+
+        if ($abstract) {
+          $pub->add_to_abstracts({text => $abstract});
+        }
+      }
+    }
   }
 
   return;
