f04a287a2cf5e32764bfd903ba152e071a61ad9c
Chris Scheller <schelcj@pobox.com>
2015-02-12 08:35:45 -0500

    keep our own list of useragent strings

--- lib/perl5/Biostat/Publications/Constants.pm
+++ lib/perl5/Biostat/Publications/Constants.pm
@@ -15,6 +15,7 @@ our @EXPORT_OK = (
     $SEMICOLON
     $TRUE
     $FALSE
+    %USER_AGENT_ALIASES
     )
 );
 
@@ -31,6 +32,7 @@ our %EXPORT_TAGS = (
       $SEMICOLON
       $TRUE
       $FALSE
+      %USER_AGENT_ALIASES
       )
   ],
 );
@@ -46,4 +48,14 @@ Readonly::Scalar our $SEMICOLON => q{;};
 Readonly::Scalar our $TRUE      => q{1};
 Readonly::Scalar our $FALSE     => q{0};
 
+Readonly::Hash our %USER_AGENT_ALIASES => (
+  'Windows IE 6'        => 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+  'Windows Mozilla'     => 'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4b) Gecko/20030516 Mozilla Firebird/0.6',
+  'Mac Safari'          => 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en-us) AppleWebKit/85 (KHTML, like Gecko) Safari/85',
+  'Mac Mozilla'         => 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.4a) Gecko/20030401',
+  'Linux Mozilla'       => 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.4) Gecko/20030624',
+  'Linux Konqueror'     => 'Mozilla/5.0 (compatible; Konqueror/3; Linux)',
+  'Linux Ubuntu FF'     => q{Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:20.0) Gecko/20100101 Firefox/20.0},
+  'Linux Ubuntu Chrome' => q{Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36};
+);
 1;
