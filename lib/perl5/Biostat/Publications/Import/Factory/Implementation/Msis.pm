11d40c3536ba81c843157bb5fe3b23500b3bc9a7
Chris Scheller <schelcj@pobox.com>
2015-02-12 08:36:43 -0500

    not doing caching, althought not sure why anymore

--- lib/perl5/Biostat/Publications/Import/Factory/Implementation/Msis.pm
+++ lib/perl5/Biostat/Publications/Import/Factory/Implementation/Msis.pm
@@ -1,24 +1,18 @@
 package Biostat::Publications::Import::Factory::Implementation::Msis;
 
-use Biostat::Publications::Base qw(biostat cache moose www);
+use Biostat::Publications::Base qw(biostat moose www);
 
-Readonly::Scalar my $CACHE_ROOT => qq{$Bin/../var/cache/msis};
-Readonly::Scalar my $URL_FMT    => q{https://www.umms.med.umich.edu/profile/publications/%s.json};
+Readonly::Scalar my $URL_FMT => q{https://www.umms.med.umich.edu/profile/publications/%s.json};
 
 has 'faculty_id' => (is => 'ro', isa => 'Int',               required => 1);
 has 'uniqname'   => (is => 'ro', isa => 'Str',               required => 1);
 has 'gid'        => (is => 'ro', isa => 'Str',               required => 1);
 has 'url'        => (is => 'ro', isa => 'Str',               lazy     => 1, builder => '_build_url');
-has 'cache'      => (is => 'ro', isa => 'Cache::File',       lazy     => 1, builder => '_build_cache');
 has 'raw'        => (is => 'ro', isa => 'Str',               lazy     => 1, builder => '_build_raw');
 has 'articles'   => (is => 'ro', isa => 'ArrayRef[HashRef]', lazy     => 1, builder => '_build_articles');
 
 sub _build_url {
   return sprintf $URL_FMT, shift->uniqname;
-}
-
-sub _build_cache {
-  return Cache::File->new(cache_root => $CACHE_ROOT);
 }
 
 sub _build_raw {
@@ -38,21 +32,22 @@ sub get_publications {
 
   my @publications = ();
   for my $article (@{$self->articles}) {
-    push @publications, Biostat::Publication->new(
-      faculty_id => $self->faculty_id,
-      src_url    => $self->url,
-      scival_url => $article->{scivalURL},
-      title      => $article->{title},
-      journal    => $article->{journalTitle},
-      volume     => $article->{journalVolume},
-      issue      => $article->{journalIssue},
-      pages      => $article->{pages},
-      authors    => $article->{author},
-      year       => $article->{year},
-      timescited => $article->{timescited},
-      pmid       => $article->{pmid},
-      scopuseid  => $article->{scopusEID},
-    );
+    push @publications,
+      Biostat::Publication->new(
+        faculty_id => $self->faculty_id,
+        src_url    => $self->url,
+        scival_url => $article->{scivalURL},
+        title      => $article->{title},
+        journal    => $article->{journalTitle},
+        volume     => $article->{journalVolume},
+        issue      => $article->{journalIssue},
+        pages      => $article->{pages},
+        authors    => $article->{author},
+        year       => $article->{year},
+        timescited => $article->{timescited},
+        pmid       => $article->{pmid},
+        scopuseid  => $article->{scopusEID},
+      );
   }
 
   return wantarray ? @publications : \@publications;
