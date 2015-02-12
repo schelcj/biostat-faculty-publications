e7b445941f95c9f1f077a0f9184e002884d3c474
Chris Scheller <schelcj@pobox.com>
2015-02-12 08:37:52 -0500

    dropped the caching code, and started setting up but putting on hold. google changed scholar and i will have to use phantomjs or something like that to get the next page links and such.

--- lib/perl5/Biostat/Publications/Import/Factory/Implementation/Google.pm
+++ lib/perl5/Biostat/Publications/Import/Factory/Implementation/Google.pm
@@ -1,17 +1,26 @@
 package Biostat::Publications::Import::Factory::Implementation::Google;
 
-use Biostat::Publications::Base qw(biostat cache moose www);
+use Biostat::Publications::Base qw(biostat moose www);
 
-Readonly::Scalar my $CACHE_ROOT => qq{$Bin/../var/cache/google};
+Readonly::Scalar my $BASE_URL          => q{http://scholar.google.com};
+Readonly::Scalar my $CITE_LIST_URL_FMT => q{/citations?hl=en&pagesize=100&user=%s};
 
-has 'faculty_id' => (is => 'ro', isa => 'Int',         required => 1);
-has 'uniqname'   => (is => 'ro', isa => 'Str',         required => 1);
-has 'gid'        => (is => 'ro', isa => 'Str',         required => 1);
-has 'cache'      => (is => 'ro', isa => 'Cache::File', lazy     => 1, builder => '_build_cache');
+Readonly::Hash my $CSS => {
+  item      => q{td#col-title a.cit-dark-large-link},
+  title     => q{div#main_sec.g-section div.cit-dl div.cit-dd div#title a},
+  title_alt => q{div#main_sec.g-section div.cit-dl div.cit-dd div#title},
+  authors   => q{div#main_sec.g-section div.cit-dl div.g-section div.cit-dd},
+  pub_date  => q{div#main_sec.g-section div.cit-dl div#pubdate_sec.g-section div.cit-dd},
+  journal   => q{div#main_sec.g-section div.cit-dl div#venue_sec.g-section div.cit-dd},
+  volume    => q{div#main_sec.g-section div.cit-dl div#volume_sec.g-section div.cit-dd},
+  issue     => q{div#main_sec.g-section div.cit-dl div#issue_sec.g-section div.cit-dd},
+  pages     => q{div#main_sec.g-section div.cit-dl div#pages_sec.g-section div.cit-dd},
+  next_lnk  => q{a.cit-dark-link},
+};
 
-sub _build_cache {
-  return Cache::File->new(cache_root => $CACHE_ROOT);
-}
+has 'faculty_id' => (is => 'ro', isa => 'Int', required => 1);
+has 'uniqname'   => (is => 'ro', isa => 'Str', required => 1);
+has 'gid'        => (is => 'ro', isa => 'Str', required => 1);
 
 sub get_publications {
   my ($self) = @_;
